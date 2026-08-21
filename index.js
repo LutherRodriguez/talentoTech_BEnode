d692c8becd2975ca0d452a4823f72568de810a81
Luther Rodríguez <luthertrabajoarg@gmail.com>
2026-08-20 21:05:52 -0300

    novedades de la 2da clase

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola, curso de BE con Node.js");
+/*console.log("Hola, curso de BE con Node.js"); // Imprime un mensaje en la consola
+console.log(process.platform);// Despliega la plataforma del sistema operativo
+console.log(process.version);// Despliega la versión de Node.js
+console.log(process.arch);// Despliega la arquitectura del sistema operativo
+console.log(process.argv); // Despliega los argumentos que se pasan al script de Node.js
+console.log(process.env); // Despliega las variables
+console.log(process.pid); // Despliega el ID del proceso
+console.log(process.cwd()); // Despliega el directorio de trabajo actual
+console.log(process.memoryUsage()); // Despliega el uso de memoria del proceso
+console.log(process.uptime()); // Despliega el tiempo de ejecución del proceso 
+console.log(process.exitCode); // Despliega el código de salida del proceso 
+console.log(process.exit()); // Finaliza el proceso de Node.js
+console.log(process.kill()); // Envía una señal a un proceso
+console.log(process.on()); // Escucha eventos del proceso
+console.log(process.stdin); // Despliega el flujo de entrada estándar
+console.log(process.stdout); // Despliega el flujo de salida estándar
+*/
